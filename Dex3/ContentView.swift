a3cc7c6c381765287334d68674c047588b803811
lyleverythinG <75658617+lyleverythinG@users.noreply.github.com>
2024-12-09 21:00:53 +0800

    Add initial view for content view (#2)

--- Dex3/ContentView.swift
+++ Dex3/ContentView.swift
@@ -6,21 +6,41 @@
 //
 
 import SwiftUI
-
-    // TODO: Setup ContentView.
+import CoreData
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Pokemon.id, ascending: true)],
+        animation: .default)
+    private var pokedex: FetchedResults<Pokemon>
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(pokedex) { pokemon in
+                    NavigationLink {
+                        Text("\(pokemon.id): \(pokemon.name!.capitalized)")
+                    } label: {
+                        Text("\(pokemon.id): \(pokemon.name!.capitalized)")
+                    }
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    EditButton()
+                }
+            }
+            Text("Select an item")
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
 }
+
