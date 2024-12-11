43ae4fb4355e1decfba63dede336c249f956c226
Lyle Dane Carcedo <75658617+lyleverythinG@users.noreply.github.com>
2024-12-11 18:21:01 +0800

    Start adding view to pokemon detail screen

--- Dex3/PokemonDetail.swift
+++ Dex3/PokemonDetail.swift
@@ -10,9 +10,61 @@ import CoreData
 
 struct PokemonDetail: View {
     @EnvironmentObject var pokemon: Pokemon
+    @State var showShiny = false
     
     var body: some View {
-        Text("HELLO WORLD")
+        ScrollView {
+            ZStack {
+                Image("normalgrasselectricpoisonfairy")
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(color: .black, radius: 6)
+                
+                AsyncImage(url:showShiny ? pokemon.shiny :  pokemon.sprite) { image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                        .padding(.top, 50)
+                        .shadow(color: .black, radius: 6)
+                } placeholder: {
+                    ProgressView()
+                }
+            }
+            
+            HStack {
+                ForEach(pokemon.types!, id: \.self) {type in
+                    Text(type.capitalized)
+                        .font(.title2)
+                        .shadow(color: .white, radius: 1)
+                        .padding([.top, .bottom], 7)
+                        .padding([.leading,.trailing])
+                        .background(Color(type.capitalized))
+                        .cornerRadius(50)
+                }
+                
+                Spacer()
+            }
+            .padding()
+        }
+        .navigationTitle(pokemon.name!.capitalized)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button {
+                    Button {
+                        showShiny.toggle()
+                    } label: {
+                        if showShiny {
+                            Image(systemName: "wand.and.stars")
+                                .foregroundColor(.yellow)
+                        } else {
+                            Image(systemName: "wand.and.stars.inverse")
+                        }
+                    }
+                } label: {
+                    
+                }
+            }
+        }
     }
 }
 
