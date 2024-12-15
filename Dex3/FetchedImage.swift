fb5b81d4eaa4f4f9b54f254a2753146c9b42776e
Lyle Dane Carcedo <75658617+lyleverythinG@users.noreply.github.com>
2024-12-15 14:45:42 +0800

    Add widget pokemon view

---
+++ Dex3/FetchedImage.swift
@@ -0,0 +1,27 @@
+//
+//  FetchedImage.swift
+//  Dex3
+//
+//  Created by Lyle Dane Carcedo on 12/15/24.
+//
+
+import SwiftUI
+
+struct FetchedImage: View {
+    let url: URL?
+    
+    var body: some View {
+        if let url, let imageData = try? Data(contentsOf: url), let uiImage = UIImage(data: imageData) {
+            Image(uiImage: uiImage)
+                .resizable()
+                .scaledToFit()
+                .shadow(color: .black, radius: 6)
+        } else  {
+            Image("bulbasaur")
+        }
+    }
+}
+
+#Preview {
+    FetchedImage(url: SamplePokemon.samplePokemon.sprite)
+}
